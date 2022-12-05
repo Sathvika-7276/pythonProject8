0465205c90a1c6010db6af4f3fcdf0fc290b303e
Sathvika-7284 <sathvika7276@gmail.com>
2022-12-05 16:37:47 +0800

    Complete prac 5 state names exercise

--- state_names.py
+++ state_names.py
@@ -3,7 +3,6 @@ CP1404/CP5632 Practical
 State names in a dictionary
 File needs reformatting
 """
-
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
