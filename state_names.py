077786f853cdfb486ad40bb8da8c4d46d24012b6
Sathvika-7284 <sathvika7276@gmail.com>
2022-12-05 16:36:31 +0800

    Working with dictionaries country code, country

--- state_names.py
+++ state_names.py
@@ -5,14 +5,17 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD ": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
+for key in CODE_TO_NAME:
+    value = CODE_TO_NAME[key]
+    print(f"{key} is {value}")
 
-state_code = input("Enter short state: ")
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
-    state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
+for i in CODE_TO_NAME:
+    try:
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+    except KeyError:
+        print("Invalid")
+    state_code = input("Enter short state: ").upper()
